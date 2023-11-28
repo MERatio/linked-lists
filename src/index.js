54c81e7c466201dee2b151c97e79d3421a23e805
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-11-28 13:15:45 +0800

    Update console.log statements

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ linkedList.append("b");
 linkedList.prepend("a");
 linkedList.append("c");
 
-console.log(linkedList.getSize());
-console.log(linkedList.getHead());
-console.log(linkedList.getTail());
+console.log("size", linkedList.getSize());
+console.log("head", linkedList.getHead());
+console.log("tail", linkedList.getTail());
