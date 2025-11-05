130337149819085c36a4a07d04fedd3689c6387f
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-11-05 12:48:46 +0800

    Add LinkedList, Node class, LinkedList's append, and toString method

---
+++ js/LinkedList.js
@@ -0,0 +1,41 @@
+import Node from './Node.js';
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Adds a new node containing value to the end of the list.
+  // Should add a tail pointer (optional performance improvement).
+  append(value) {
+    const newNode = new Node(value, null);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let cur = this.head;
+      while (cur.nextNode !== null) {
+        cur = cur.nextNode;
+      }
+      cur.nextNode = newNode;
+    }
+  }
+
+  // Represents LinkedList objects as strings.
+  // ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
+  toString() {
+    if (this.head === null) {
+      return 'null';
+    } else {
+      let str = '';
+      let cur = this.head;
+      while (cur !== null) {
+        str += `( ${cur.value} ) -> `;
+        cur = cur.nextNode;
+      }
+      str += 'null';
+      return str;
+    }
+  }
+}
+
+export default LinkedList;
