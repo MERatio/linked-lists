5c523850ce1df8a0c79c3eded10cf363964f1399
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-11-06 10:35:25 +0800

    Change test logs

--- js/index.js
+++ js/index.js
@@ -2,19 +2,39 @@ import LinkedList from './LinkedList.js';
 
 const list = new LinkedList();
 
-list.append('d');
-list.append('e');
-list.prepend('b');
-list.prepend('a');
+// Test append
+list.append('dog');
+list.append('cat');
+list.append('parrot');
 
-console.log(list.toString());
-console.log(list.size);
-console.log('Head', list.head);
-console.log('Tail', list.tail);
-console.log('At index 2', list.at(2));
-console.log('Pop', list.pop());
-console.log(list.toString());
-console.log('contains d?', list.contains('d'));
-console.log('index of d?', list.find('d'));
-console.log(list.insertAt('c', 2), 'c inserted', list.toString());
-console.log(list.removeAt(2), 'c removed', list.toString());
+console.log(list.toString()); // ( dog ) -> ( cat ) -> ( parrot ) -> null
+console.log('Size:', list.size); // 3
+console.log('Head:', list.head.value); // dog
+console.log('Tail:', list.tail.value); // parrot
+
+// Test prepend
+list.prepend('hamster');
+console.log(list.toString()); // ( hamster ) -> ( dog ) -> ( cat ) -> ( parrot ) -> null
+
+// Test at()
+console.log('Node at index 2:', list.at(2).value); // cat
+
+// Test pop()
+console.log('Popped:', list.pop()); // parrot
+console.log(list.toString()); // ( hamster ) -> ( dog ) -> ( cat ) -> null
+
+// Test contains()
+console.log('Contains dog?', list.contains('dog')); // true
+console.log('Contains snake?', list.contains('snake')); // false
+
+// Test find()
+console.log('Index of cat:', list.find('cat')); // 2
+console.log('Index of snake:', list.find('snake')); // null
+
+// Test insertAt()
+list.insertAt('snake', 2);
+console.log(list.toString()); // ( hamster ) -> ( dog ) -> ( snake ) -> ( cat ) -> null
+
+// Test removeAt()
+console.log('Removed:', list.removeAt(1)); // dog
+console.log(list.toString()); // ( hamster ) -> ( snake ) -> ( cat ) -> null
